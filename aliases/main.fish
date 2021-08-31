1f2f264f317dd1e2c6aaded3a47dbaa9f57038e8
Aleksandr Iotmalik <siretna@gmail.com>
2021-08-31 18:22:17 +0300

    aliases cleaned

--- aliases/main.fish
+++ aliases/main.fish
@@ -1,11 +1,8 @@
-## Projects
-alias freshauto="cd ~/projects/freshauto"
-alias smartomato="cd ~/projects/smartomato"
-
-## Project envs
-alias freshauto_env="brew services stop postgresql | brew services start postgresql@11"
-alias smartomato_env="brew services stop postgresql@11 | brew services start postgresql"
-
 ## Ruby
 alias run_faye="rackup faye.ru --host 0.0.0.0 -E production"
 # alias run_sidekiq="sidekiq -C config/sidekiq.yml"
+
+# OLD
+## Project envs
+# alias env1="brew services stop postgresql | brew services start postgresql@11"
+# alias env2="brew services stop postgresql@11 | brew services start postgresql"
