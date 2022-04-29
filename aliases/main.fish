4e044d973bee31df0d64e8dbdfa2a6709dd53fc6
Aleksandr <siretna@gmail.com>
2022-04-29 16:04:43 +0700

    old cheetsheets (just in case)

--- aliases/main.fish
+++ aliases/main.fish
@@ -4,9 +4,11 @@ alias rubocop_autocorrect="bundle exec rubocop -c .rubocop.yml -A"
 
 # alias run_faye="rackup faye.ru --host 0.0.0.0 -E production"
 # alias run_sidekiq="sidekiq -C config/sidekiq.yml"
+# alias elasticsearch_recreate_indexes="rake environment elasticsearch:import:all FORCE=y"
 
 ## Git
 alias git_hotfix="git status && git commit --amend --no-edit && git push --force"
+alias git_delete_local_branches="git checkout master && git branch | grep -v '^*' | xargs git branch -D"
 
 ## dirs
 alias fish_dir="cd ~/.config/fish"
