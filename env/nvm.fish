ef61d79d0258626bd4848cd07f165488e2d79eec
Aleksandr <siretna@gmail.com>
2022-10-10 14:09:08 +0400

    Check nvmrc to set nvm node vesrion

--- env/nvm.fish
+++ env/nvm.fish
@@ -7,3 +7,22 @@ nvm use default --silent
 
 # mac os M1 issue
 # set -gx NODE_OPTIONS "--openssl-legacy-provider"
+
+# .nvmrc
+function __check_nvm --on-variable PWD --description 'Detect node version'
+  if test -f .nvmrc
+    set node_version (nvm version)
+    set nvmrc_node_version (nvm version (cat .nvmrc))
+
+    if [ $nvmrc_node_version = "N/A" ]
+      nvm install
+    else if [ $nvmrc_node_version != $node_version ]
+      nvm use
+    end
+
+    set -gx X_NVM_DIRTY_FLAG 1
+  else if set -q X_NVM_DIRTY_FLAG; and [ (node --version) != (nvm version default) ]
+    nvm use default
+    set -e X_NVM_DIRTY_FLAG
+  end
+end
